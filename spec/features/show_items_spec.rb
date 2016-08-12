674c18c030d32fa97b63c78415b8cf22f4d23902
johshisha <johshisha@gmail.com>
2016-08-13 02:28:01 +0900

    recommended test

--- spec/features/show_items_spec.rb
+++ spec/features/show_items_spec.rb
@@ -5,9 +5,20 @@ RSpec.feature "See shop items", type: :feature do
     create(:item, name: '包丁')
     create(:item, name: 'フライパン')
 
-    visit '/items'
+    visit items_path
 
     expect(page).to have_text('包丁')
     expect(page).to have_text('フライパン')
   end
+  
+  scenario 'A user can see various items' do
+    create(:item, name: '包丁', recommended: true)
+    create(:item, name: 'フライパン')
+
+    visit recommended_items_path
+
+    expect(page).to have_text('包丁')
+    expect(page).not_to have_text('フライパン')
+  end
+
 end
