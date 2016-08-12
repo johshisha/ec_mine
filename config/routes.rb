da0faa5caacb501026df34568917bc1052abca93
johshisha <johshisha@gmail.com>
2016-08-13 02:48:07 +0900

    カテゴリ分け

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :items, only: %i(index show)
+  resources :items, only: %i(index show) do
+    collection do
+      get :recommended
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
